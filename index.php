e74b3aa2e1f593bd74802848bf7f8b347f23f773
ibrahimlawal <ibrahim@lawal.me>
2016-02-09 01:59:07 +0100

    test webhook 2

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ require __DIR__ . '/vendor/autoload.php';
 $paystack = new Eidetic\Paystack('sk_test_40899660eac2be0a6a6915f6ba32f81bc8bac143');
 
 print_r($paystack->customer(199));
-print_r($paystack->customer(200));
+print_r($paystack->customer(20));
 
